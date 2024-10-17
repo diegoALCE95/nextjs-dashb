130e46e32017d564166b2af2c861fcaf25386c33
Diego Alvarez <diegoalvarezc95@gmail.com>
2024-10-17 03:09:18 -0500

    chapter 10

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,5 +1,18 @@
-export default function Page() {
-    return (
-      <div>Invoices Page</div>
-    )
-  }
+import { Suspense } from 'react';
+import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
+import { LatestInvoicesSkeleton } from '@/app/ui/skeletons';
+
+
+import { 
+  fetchLatestInvoices
+} from '@/app/lib/data';
+
+export default async function Page() {
+  const latestInvoices = await fetchLatestInvoices();
+
+  return (
+    <Suspense fallback={<LatestInvoicesSkeleton />}>
+      <LatestInvoices />
+    </Suspense>
+  )
+}
