51ab76740d8f0acd6477d367203c0ad89f2d212c
Diego Alvarez <diegoalvarezc95@gmail.com>
2024-11-30 17:04:35 -0500

    added update and delete funcs to invoices

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -5,6 +5,9 @@ import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 
+/**
+ * Create Invoice
+ */
  
 const FormSchema = z.object({
     id: z.string(),
@@ -15,8 +18,6 @@ const FormSchema = z.object({
 });
  
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
-
-/****/
  
 export async function createInvoice(formData: FormData) {
 
@@ -38,3 +39,35 @@ export async function createInvoice(formData: FormData) {
     redirect('/dashboard/invoices');
 }
 
+/**
+ * Update Invoice
+ */
+
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
+ 
+export async function updateInvoice(id: string, formData: FormData) {
+  const { customerId, amount, status } = UpdateInvoice.parse({
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  const amountInCents = amount * 100;
+ 
+  await sql`
+    UPDATE invoices
+    SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
+    WHERE id = ${id}
+  `;
+ 
+  revalidatePath('/dashboard/invoices');
+  redirect('/dashboard/invoices');
+}
+
+/**
+ * Delete Invoice
+ */
+export async function deleteInvoice(id: string) {
+    await sql`DELETE FROM invoices WHERE id = ${id}`;
+    revalidatePath('/dashboard/invoices');
+}
